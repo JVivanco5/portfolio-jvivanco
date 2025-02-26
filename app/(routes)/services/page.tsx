bb7924821da09556a127426e0f2476729f733a22
JVivanco5 <joaq.vivanco@duocuc.cl>
2025-02-26 18:49:30 -0300

    Corrección header superpuesto en mis conocimientos

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -8,34 +8,25 @@ const ServicesPage = () => {
   return (
     <>
       <TransitionPage />
-      {/* <CircleImage />
-      <AvatarServices /> */}
       <CoverParticles />
-      <div className="p-6 grid items-center justify-center h-screen max-w-5xl gap-6 mx-auto md:grid-cols-2 md:px-20">
-
-        <div className="max-w[400px] mt-20 md:mt-0">
-
+      {/* Se agrega un padding top para separar el contenido del header */}
+      <div className="pt-24 p-6 grid items-center justify-center h-screen max-w-5xl gap-6 mx-auto md:grid-cols-2 md:px-20">
+        <div className="max-w-[400px] mt-20 md:mt-0">
           <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5">
-            Mis{' '}
-
-            <span className="font-bold text-secondary">Conocimientos.</span>
-
+            Mis{" "}
+            <span className="font-bold text-secondary">Conocimientos</span>
           </h1>
-
-          <p className="mb-3 text-xl text-gray-300">Te presentos lenguajes y tecnologias que manejo.
-            Me encuentro constantemente estudiando para seguir adquiriendo conocimientos.</p>
-
+          <p className="mb-3 text-xl text-gray-300">
+            Te presento lenguajes y tecnologías que manejo. Me encuentro constantemente
+            estudiando para seguir adquiriendo conocimientos.
+          </p>
         </div>
 
         {/* Slider */}
         <div>
-
           <SliderServices />
-
         </div>
-
       </div>
-
     </>
   );
 }
